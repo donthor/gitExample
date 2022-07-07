5f9ee82fd0dea7bb56cde61c89a835037f6b40de
donthor <parockho@cisco.com>
2022-07-07 19:26:12 -0400

    changed to Evaluating

--- numdouble.py
+++ numdouble.py
@@ -2,6 +2,6 @@ def doubler(number):
     result = number * 2
     return result
 get_number = float(input("Type a number: "))
-print('Geeting your answer')
+print('Evaluating your answer')
 answer = doubler(get_number)
 print(f'Your correct answer is: {answer}')
