6d1aaae871ebe824121721b8b652bc46b945c4b2
donthor <parockho@cisco.com>
2022-07-07 19:08:44 -0400

    first commit

---
+++ numdouble.py
@@ -0,0 +1,7 @@
+def doubler(number):
+    result = number * 2
+    return result
+get_number = float(input("Type a number: "))
+print('Geeting your answer')
+answer = doubler(get_number)
+print(answer)
